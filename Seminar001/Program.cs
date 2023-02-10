632da8ab38ecb5a74a00d06d3ba165c85fee1e17
ALGUL1987 <tankipanki1@gmail.com>
2023-02-10 20:59:13 +0300

    Seminar001

---
+++ Seminar001/Program.cs
@@ -0,0 +1,7 @@
+﻿/* Напишите программу, которая на вход принимает число и выдает его квадрат*/
+/* тип_данных имя_переменной = значение */
+
+int number = 5; // number=5
+int result = number * number; // result = number * number //
+
+Console.WriteLine("Квадрат от числа: " + number + " равен " + result);
