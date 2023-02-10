9a43dfec342608e9d3ca17092903ab0c729002cd
ALGUL1987 <tankipanki1@gmail.com>
2023-02-10 21:11:43 +0300

    Seminar001_Convert

--- Seminar001/Program.cs
+++ Seminar001/Program.cs
@@ -1,7 +1,12 @@
 ﻿/* Напишите программу, которая на вход принимает число и выдает его квадрат*/
 /* тип_данных имя_переменной = значение */
 
-int number = 5; // number=5
+
+/* Введите число, для которого мы ищем квадрат */
+
+Console.WriteLine("Введите число, для которого мы ищем квадрат: ");
+
+int number = Convert.ToInt32(Console.ReadLine()); // number=5 */ Convert преобразует данные в требуемый параметр */
 int result = number * number; // result = number * number //
 
 Console.WriteLine("Квадрат от числа: " + number + " равен " + result);
